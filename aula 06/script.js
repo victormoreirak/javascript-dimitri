d0bb83a9c318e8b8106a0195edd175d7f94bf7ca
Victor Moreira <136837606+victormoreirak@users.noreply.github.com>
2023-08-08 16:24:16 -0300

    aula 06 var let const

--- aula 06/script.js
+++ aula 06/script.js
@@ -1,1 +1,31 @@
+/* 
+Vamos entender variáveis
 
+No javascript temos 4 palavras-chaves para declarar variáveis:
+
+-> var
+-> let
+-> const
+*/
+
+/*
+DECLARAÇÃO DE VARIAVEIS
+var a, b, c;
+
+ATRIBUIÇÃO DOS VALORES
+a = 2;
+b = 3;
+c = a + b;
+
+var a = 5; //com "var" é possível reatribuir valores, com let e const não 
+alert(c);*/
+
+// DECLARAÇÃO DE VARIÁVEIS
+var nome, sobrenome, nomeCompleto;
+
+// ATRIBUIÇÃO DOS VALORES
+nome = "Dimitri";
+sobrenome = "Teixeira";
+nomeCompleto = nome + sobrenome;
+
+document.getElementById("texto").innerHTML = nomeCompleto;
