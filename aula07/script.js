eef724f4b9fb28aa4ecc12343ae475b20259d733
Victor Moreira <136837606+victormoreirak@users.noreply.github.com>
2023-08-24 18:00:42 -0300

    operadores aritméticos, atribuição e sequência

--- aula07/script.js
+++ aula07/script.js
@@ -14,4 +14,43 @@ São separados em 6 "categorias":
 5) Operador Condicional (Ternário)
 6) Operadores Lógicos
 
+
+1) Operadores Aritméticos (matématicos)
+
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 2;
+
+total = valor1 + valor2; // operador aritmético (+) e atribuição (=) 
+alert(total);
+
+2) Operadores de Atribuição
+
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 2;
+
+total = ++valor1; // operador aritmético (+) e atribuição (=) 
+alert('O valor1 ficou: ' + valor1); // operador de sequência (concatenação)
+
+
+
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 2;
+
+valor1 += valor2;
+valor1 = valor1 + valor2; // mesma operação (também serve para - * /)
+
+
+
+3) Operadores de Sequência
+
+var valor1, valor2, total;
+valor1 = "Victor ";
+valor2 = "Moreira";
+
+total = valor1 + valor2;
+alert(total);
+
 */
