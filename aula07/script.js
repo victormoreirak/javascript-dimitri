19396179499dae07344c21605caf83e48d7a9b43
Victor Moreira <136837606+victormoreirak@users.noreply.github.com>
2023-08-24 18:38:13 -0300

    operadores de comparação, condicional e lógicos

--- aula07/script.js
+++ aula07/script.js
@@ -53,4 +53,55 @@ valor2 = "Moreira";
 total = valor1 + valor2;
 alert(total);
 
+
+4) Operadores de Comparação
+
+var valor1, valor2, total;
+valor1 = 8;
+valor2 = 10;
+
+total = (valor1 == valor2);
+alert(total);
+
+
+var valor1, valor2, total;
+valor1 = 8;
+valor2 = 12;
+
+total = (valor1 <= 9) // true (verdadeiro) ou false (falso);
+alert(total);
+
+
+5) Operador Condicional (Ternário)
+
+var idade, eleitor;
+idade=18;
+eleitor = (idade < 18) ? "Não é eleitor" : "Sim, é eleitor";
+
+alert('A resposta é: ' + eleitor + '! A idade dele é de: ' + idade + ' anos');
+
+
+
+6) Operadores Lógicos // quano precisa de mais de 1 condição
+
+
+
+var idade, eleitor, resultado;
+idade=63;
+eleitor = (idade < 18) ? "Não é eleitor" : "Sim, é eleitor";
+
+resultado = (idade > 60 && idade < 70); // retorna true, senão false
+
+alert(resultado)
+
+
+
+
+var idade, eleitor, resultado;
+idade=63;
+eleitor = (idade < 18) ? "Não é eleitor" : "Sim, é eleitor";
+
+resultado = !(idade === 65 || idade === 72); // retorna true, senão false
+
+alert(resultado);
 */
