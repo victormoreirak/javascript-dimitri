2db4f43034519b0ac2bb58e538168a2c39f274df
Victor Moreira <136837606+victormoreirak@users.noreply.github.com>
2023-08-28 20:17:52 -0300

    aula 10 - eventos

--- aula10/script.js
+++ aula10/script.js
@@ -38,3 +38,12 @@ function viraVermelho() {
     let div = document.getElementById("teste");
     div.style.backgroundColor = "red"
 }
+
+function viraAzul() {
+    let div = document.getElementById("teste");
+    div.style.backgrounColor = "blue"
+}
+function adicionaTexto() {
+    let p = document.getElementById("texto");
+    p.append('O mouse moveu<br>');
+}
