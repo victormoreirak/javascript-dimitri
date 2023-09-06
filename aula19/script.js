75abc6f5e2eefa746214e9c3010c3bfcac6cd0ff
Victor Moreira <136837606+victormoreirak@users.noreply.github.com>
2023-09-06 17:49:13 -0300

    aula 19 - json

---
+++ aula19/script.js
@@ -0,0 +1,16 @@
+/**
+ 
+JSON
+
+JSON significa Javascript Object Notation, que traduzido para o português fica algo como notação de objeto Javascript.
+
+Explicando de um modo simples, JSON é basicamente uma forma de converter um objeto em texto e o contrário também, um texto em um objeto.
+
+Ele é usado principalmente para transmitir dados entre sistemas de forma simples, já que o formato de texto é lido por praticamente toda linguagem de programação.
+
+Para trabalhar com JSON no Javascript usamos dois métodos:
+
+JSON.parse() -> Converte texto no padrão JSON em objetos
+JSON.stringify() -> Converte objetos em texto padrão JSON
+
+ */
